2765a72c409aa13aedf65ab87397b58e21f8ce3e
Jason Anderson <jason@gearsandwires.com>
2021-01-11 14:15:54 -0800

    2.1.4

--- examples/src/DisplayTextDemo/index.tsx
+++ examples/src/DisplayTextDemo/index.tsx
@@ -7,7 +7,9 @@ import {
 } from "../../../src";
 import { ReactDemoComponentProps } from "../_internal/ReactDemoContainer";
 
-const demoText = `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`;
+const demoText = `Welcome to my meme shop!
+
+Make your choice and I'll send you a meme. You can let me know in the description if you want a specific theme or just a random one.`;
 
 export class DisplayTextDemo extends React.Component<
   ReactDemoComponentProps,
